a935c72d37a69b49c189e708053216875b4d46f3
christophe maggiore <chrismag06000@gmail.com>
2024-10-08 16:31:35 +0200

    add create table

---
+++ CreateTablelpecom_rpps.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE lpecom_rpps
+(
+    id INT PRIMARY KEY NOT NULL,
+    id_pp_nat  INT
+    nom VARCHAR(100),
+    prenom VARCHAR(100),
+    code_profession INT
+    lib_profession VARCHAR(100),
+    code_savoir_faire VARCHAR(100),
+    lib_savoir_faire VARCHAR(100),
+    code_postal INT
+)
---
+++ Ex-VII-Part07.sql
@@ -0,0 +1,12 @@
+-- Initiation SQL - Exercices VII / Septième partie
+
+-- Exercice 1
+-- Quelle requête SQL utiliser pour compter, sans doublons, le nombre de professionnels de santé en
+-- Seine-et-Marne (77) ?
+
+
+-- Exercice 2
+-- Quelle requête SQL utiliser pour afficher pour tous les professionnels de santé avec le code postal
+-- 77300 les colonnes suivantes :
+-- id_pp_nat, prenom, nom, code_postal, ville, departement et région. Vous aurez besoin de plusieurs
+-- jointures
